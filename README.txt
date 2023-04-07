7c1299a5f3cc6a5ff26950ea8b2d6989ab357b4f
Simão Vendeirinho <simaopvendeirinho@gmail.com>
2023-04-07 13:00:37 +0100

    update

--- README.txt
+++
@@ -1,3 +0,0 @@
-A usar os pacotes: chart.js react-chartjs-2 react-icons tailwindcss
-
-Necessário instalar para não dar erro.
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex justify-between px-4 pt-4'>
-      <h2>ISCF Lab1</h2>
+      <h2>ISCF Lab1 by Simão Vendeirinho & João Potier</h2>
       <h2>2022/2023 NOVA SST</h2>
     </div>
   )
